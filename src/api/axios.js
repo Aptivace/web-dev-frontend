8076650ded8cecb9211e42bf554d9b363431220a
Aptivace <yarovikov758@gmail.com>
2025-03-08 19:05:15 +0300

    add Interseptors

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,32 @@
 import axios from "axios";
 
-export default axios.create({
+const api = axios.create({
   baseURL: "http://localhost:8000/api",
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true; // Помечаем запрос как повторный
+      try {
+        const res = await axios.post(
+          "http://localhost:8000/api/auth/refresh",
+          null,
+          { withCredentials: true }
+        );
+        // Повторяем оригинальный запрос с новым токеном
+        return api(originalRequest);
+      } catch (refreshError) {
+        // Если обновление токена не удалось, перенаправляем на страницу входа
+        window.location.href = "/#/login";
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
